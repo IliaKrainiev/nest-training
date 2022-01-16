8fe4afa08dcfc6e211231dc505f435aaeed61f31
ikrainiev <ikrainiev@star.global>
2022-01-16 17:33:46 +0200

    l3

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,16 +1,39 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
   @Get()
-  findAll() {}
-  @Get()
-  findOne() {}
+  findAll() {
+    return this.events;
+  }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    const event = this.events.find((event) => event.id === parseInt(id));
+    return id;
+  }
   @Post()
-  create() {}
-  @Patch()
-  update() {}
+  create(@Body() input: CreateEventDto) {
+    return input;
+  }
+  @Patch(':id')
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    return input;
+  }
   @Delete()
-  delete() {}
+  @HttpCode(204)
+  remove(@Param('id') id) {}
 }
